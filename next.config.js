c7cf105781bece53bcbff6dc0797fef556bbdb04
Namoj-design <namojperiakumar@gmail.com>
2026-04-10 02:03:50 +0530

    Remove legacy next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    serverActions: { bodySizeLimit: '2mb' },
-  },
-};
-
-module.exports = nextConfig;
